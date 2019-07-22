a148e08dd4d3438b228e4a6c268be5ba9943f5ee
Taha <baktash.29.ra@gmail.com>
2019-07-22 18:03:53 +0800

    end of the day

--- D3_exercises/01-Click-me/lib/index.js
+++ D3_exercises/01-Click-me/lib/index.js
@@ -1,8 +1,12 @@
 const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
-
+  var button = document.querySelector(".btn-success");
   // TODO: Bind the `click` event to the button
+  button.addEventListener("click", sayThanks);
   // TODO: On click, display `Thank you!` in a JavaScript alert!
+  function sayThanks(){
+    alert("Thank You");
+  }
 };
 
 displayAlertOnButtonClick(); // Do not remove!
--- D3_exercises/04-Recode-chat/lib/index.js
+++ D3_exercises/04-Recode-chat/lib/index.js
@@ -1,4 +1,27 @@
-const batch = 1; // change to your own batch id
+const batch = "general"; // change to your own batch id
 const baseUrl = "https://recode-chat.herokuapp.com/";
 
 // Your turn to code!
+var chatForm = document.querySelector("#comment-form");
+var senderMessage = document.querySelector("textarea");
+var senderName = document.querySelector("input");
+var messages = document.querySelector("input");
+var messagesList = document.querySelector("ul");
+
+chatForm.addEventListener("submit", sendMessage);
+chatForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+function sendMessage() {
+  var message = chatForm[0].value;
+  var sender = chatForm[1].value;
+  var date = new Date;
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode(`${message} (by ${sender}) (Posted on ${date}) `);
+  node.appendChild(textnode);
+  messagesList.appendChild(node);
+}
+
+fetch(baseUrl);
+.then()

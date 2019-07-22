cb3b6df3b79731864444566f264785449674c643
Taha <baktash.29.ra@gmail.com>
2019-07-22 11:02:29 +0800

    Added ex 1 & 2

--- D3_exercises/02-Highlights/lib/index.js
+++ D3_exercises/02-Highlights/lib/index.js
@@ -1,1 +1,18 @@
 // TODO: write your code here!
+var football = document.querySelectorAll(".clickable")[0];
+var volleyball = document.querySelectorAll(".clickable")[1];
+var basketball = document.querySelectorAll(".clickable")[2];
+var baseball = document.querySelectorAll(".clickable")[3];
+var pingpong = document.querySelectorAll(".clickable")[4];
+var hiking = document.querySelectorAll(".clickable")[5];
+
+football.addEventListener("click", function() { addBorder(football); });
+volleyball.addEventListener("click", function() { addBorder(volleyball); });
+basketball.addEventListener("click", function() { addBorder(basketball); });
+baseball.addEventListener("click", function() { addBorder(baseball); });
+pingpong.addEventListener("click", function() { addBorder(pingpong); });
+hiking.addEventListener("click", function() { addBorder(hiking); });
+
+function addBorder(clickableItem) {
+    clickableItem.classList.add("active");
+}
